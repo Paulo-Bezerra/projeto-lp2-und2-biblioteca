43fe369cdbec73e75ec8f6cabf85f39647686001
Paulo Bezerra <43281474+Paulo-Bezerra@users.noreply.github.com>
2024-06-06 12:34:27 -0300

    Cria a classe pessoa como abstrata.
    A classe pessoa será herdade por Estudante, Professor e Bibliotecário.

---
+++ src/modelo/Pessoa.java
@@ -0,0 +1,50 @@
+package modelo;
+
+import java.time.LocalDate;
+
+public abstract class Pessoa {
+    private String nome;
+    private String cpf;
+    private String matricula;
+    private LocalDate dataNascimento;
+
+    public Pessoa(String nome, String cpf, String matricula, LocalDate dataNascimento) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.matricula = matricula;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
+
