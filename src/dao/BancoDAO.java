cbd88ed420146ada4f4a93aff2eda21c2aae3462
Paulo Bezerra <43281474+Paulo-Bezerra@users.noreply.github.com>
2024-06-06 20:45:48 -0300

    Adiciona uma instância do Repositório de Estudante.

--- src/dao/BancoDAO.java
+++ src/dao/BancoDAO.java
@@ -1,14 +1,17 @@
 package dao;
 
+import repositorio.EstudanteRepositorio;
 import repositorio.LivroRepositorio;
 
 public class BancoDAO {
   private final LivroRepositorio LR;
+  private final EstudanteRepositorio ER;
 
   private static BancoDAO instance;
 
   private BancoDAO() {
     LR = new LivroRepositorio();
+    ER = new EstudanteRepositorio();
   }
 
   public static BancoDAO getInstance() {
@@ -20,5 +23,9 @@ public class BancoDAO {
 
   public LivroRepositorio getLR() {
     return LR;
+  }
+
+  public EstudanteRepositorio getER() {
+    return ER;
   }
 }
