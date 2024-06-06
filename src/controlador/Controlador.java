6e9f3ce7bdad91edc12c6f14c5c5c109a34f9542
Paulo Bezerra <43281474+Paulo-Bezerra@users.noreply.github.com>
2024-06-06 12:23:13 -0300

    Adiciona os métodos de manipulação dos livros.

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -19,10 +19,38 @@ public class Controlador {
         return op.adicionarLivro(livroDTO);
     }
 
+    public boolean removerLivro(LivroDTO livroDTO) {
+        if (!livroDTO.validarLivro()) {
+            return false;
+        }
+        return op.removerLivro(livroDTO);
+    }
+
+    public List<LivroDTO> listarLivros() {
+        return op.listarLivros();
+    }
+
+    public List<LivroDTO> buscarLivroPorTitulo(String titulo) {
+        return op.buscarLivroPorTitulo(titulo);
+    }
+
     public List<LivroDTO> buscarLivroPorAutor(String autor) {
         return op.buscarLivroPorAutor(autor);
     }
 
+    public List<LivroDTO> buscarLivroPorAssunto(String assunto) {
+        return op.buscarLivroPorAssunto(assunto);
+    }
 
+    public List<LivroDTO> buscarLivroPorAno(int ano) {
+        return op.buscarLivroPorAno(ano);
+    }
 
+    public List<LivroDTO> buscarLivrosPorEstoqueCadastrado(int estoqueCadastrado) {
+        return op.buscarLivrosPorEstoqueCadastrado(estoqueCadastrado);
+    }
+
+    public List<LivroDTO> buscarLivrosPorEstoqueDisponivel(int estoqueDisponivel) {
+        return op.buscarLivrosPorEstoqueDisponivel(estoqueDisponivel);
+    }
 }
