9190a5cfdd48a304812b461dd72e799401a35cfc
Paulo Bezerra <43281474+Paulo-Bezerra@users.noreply.github.com>
2024-06-08 19:44:16 -0300

    Cria e adiciona Credenciais a Bibliotecário.

--- src/modelo/Bibliotecario.java
+++ src/modelo/Bibliotecario.java
@@ -3,12 +3,22 @@ package modelo;
 import java.time.LocalDate;
 
 public class Bibliotecario extends Usuario {
+  private Credencias credencias;
 
-  public Bibliotecario(String nome, String cpf, String matricula, LocalDate dataNascimento) {
+  public Bibliotecario(String nome, String cpf, String matricula, LocalDate dataNascimento, Credencias credencias) {
     super(nome, cpf, matricula, dataNascimento);
+    this.credencias = credencias;
   }
 
   public Bibliotecario(Bibliotecario bibliotecario) {
     super(bibliotecario);
+    this.credencias = bibliotecario.getCredencias();
+  }
+
+  public Credencias getCredencias() {
+    return credencias;
+  }
+  public void setCredencias(Credencias credencias) {
+    this.credencias = credencias;
   }
 }
--- src/modelo/Credencias.java
+++ src/modelo/Credencias.java
@@ -1,32 +1,32 @@
 package modelo;
 
 public class Credencias {
-    private String login;
-    private String senha;
+  private String login;
+  private String senha;
 
-    public Credencias(String login, String senha) {
-        this.login = login;
-        this.senha = senha;
-    }
+  public Credencias(String login, String senha) {
+    this.login = login;
+    this.senha = senha;
+  }
 
-    public Credencias(Credencias credencias) {
-        this.login = credencias.getLogin();
-        this.senha = credencias.getSenha();
-    }
+  public Credencias(Credencias credencias) {
+    this.login = credencias.getLogin();
+    this.senha = credencias.getSenha();
+  }
 
-    public String getLogin() {
-        return login;
-    }
+  public String getLogin() {
+    return login;
+  }
 
-    public void setLogin(String login) {
-        this.login = login;
-    }
+  public void setLogin(String login) {
+    this.login = login;
+  }
 
-    public String getSenha() {
-        return senha;
-    }
+  public String getSenha() {
+    return senha;
+  }
 
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
+  public void setSenha(String senha) {
+    this.senha = senha;
+  }
 }
