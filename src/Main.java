6d3e2eced5758bc16324951c00f1c3ede0969ea0
Paulo-Bezerra <43281474+Paulo-Bezerra@users.noreply.github.com>
2024-06-08 23:30:42 -0300

    Reflete na chamada da função o ISBN  adicionado aos Livros

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     Controlador controlador = new Controlador();
-    LivroDTO livroDTO = new LivroDTO("Bom", "eu", "paz", 2024, 1, 1);
+    LivroDTO livroDTO = new LivroDTO("Bom", "eu", "1","paz", 2024, 1, 1);
     System.out.println(controlador.adicionarLivro(livroDTO));
-    livroDTO = new LivroDTO("Mau", "tu", "guerra", 2024, 9, 9);
+    livroDTO = new LivroDTO("Mau", "tu", "2", "guerra", 2024, 9, 9);
     System.out.println(controlador.adicionarLivro(livroDTO));
     System.out.println("[");
     for (LivroDTO livro : controlador.buscarLivroPorAutor("eu")) {
