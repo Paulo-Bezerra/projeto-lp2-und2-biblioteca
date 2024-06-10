57c9c2dc083bd235b7ef58dd66a5641ee744c9b6
Paulo-Bezerra <43281474+Paulo-Bezerra@users.noreply.github.com>
2024-06-10 03:29:44 -0300

    Adiciona tratamento de erro na leitura.

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -1,40 +1,4 @@
 package controlador;
 
-import dto.*;
-import servico.Operacoes;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-
 public class Controlador {
-  private final Operacoes op;
-
-  public Controlador() {
-    op = new Operacoes();
-  }
-
-  public List<LivroDTO> buscarLivroPorTitulo(String titulo) {
-    return op.buscarLivroPorTitulo(titulo);
-  }
-
-  public List<LivroDTO> buscarLivroPorAutor(String autor) {
-    return op.buscarLivroPorAutor(autor);
-  }
-
-  public List<LivroDTO> buscarLivroPorAssunto(String assunto) {
-    return op.buscarLivroPorAssunto(assunto);
-  }
-
-  public List<LivroDTO> buscarLivroPorAno(int ano) {
-    return op.buscarLivroPorAno(ano);
-  }
-
-  public List<LivroDTO> buscarLivrosPorEstoqueCadastrado(int estoqueCadastrado) {
-    return op.buscarLivrosPorEstoqueCadastrado(estoqueCadastrado);
-  }
-
-  public List<LivroDTO> buscarLivrosPorEstoqueDisponivel(int estoqueDisponivel) {
-    return op.buscarLivrosPorEstoqueDisponivel(estoqueDisponivel);
-  }
 }
--- src/util/Leitura.java
+++ src/util/Leitura.java
@@ -1,25 +1,30 @@
 package util;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Leitura {
 
-  private static Scanner sc = new Scanner(System.in);
+  private static final Scanner sc = new Scanner(System.in);
 
   public static int leInt(String msg) {
-    System.out.print(msg);
-    return sc.nextInt();
-  }
-
-  public static float leFloat(String msg) {
-    System.out.print(msg);
-    return sc.nextFloat();
+    int valor = 0;
+    boolean lido = false;
+    while (!lido) {
+      System.out.print(msg);
+      try {
+        valor = sc.nextInt();
+        lido = true;
+      } catch (InputMismatchException e) {
+        System.out.println("Erro: por favor, digite um número inteiro válido.");
+        sc.next(); // Limpa o buffer de entrada
+      }
+    }
+    return valor;
   }
 
   public static String leStr(String msg) {
     System.out.print(msg);
-    sc = new Scanner(System.in);
     return sc.nextLine();
   }
-
 }
