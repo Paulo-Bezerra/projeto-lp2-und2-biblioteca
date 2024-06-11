42c26481368a9c346a34aa9e502d08a03f18918c
Paulo Bezerra <43281474+Paulo-Bezerra@users.noreply.github.com>
2024-06-11 15:50:06 -0300

    Começa a gerenciar empréstimos.

--- src/visao/GerenciarEmprestimoUI.java
+++ src/visao/GerenciarEmprestimoUI.java
@@ -1,4 +1,34 @@
 package visao;
 
+import controlador.ControladorLivro;
+
 public class GerenciarEmprestimoUI {
+  ControladorLivro cEmprestimo = new ControladorLivro();
+
+  public void gerenciarEmprestimo() {
+    int opcao = 0;
+    do {
+      opcao = MenuUI.gerenciarEmprestimo();
+      switch (opcao) {
+        case 1:
+          registrarEmprestimo(); //
+          break;
+        case 2:
+          registrarDevolucao(); //
+          break;
+        case 3:
+          consultarEmprestimos(); // //
+          break;
+      }
+
+    } while (opcao != 4);
+  }
+
+  private void registrarEmprestimo() {
+  }
+  private void registrarDevolucao() {
+  }
+
+  private void consultarEmprestimos() {
+  }
 }
