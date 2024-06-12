fca23925ac44c234f68913c787e2a547dfdc9c4f
Paulo Bezerra <43281474+Paulo-Bezerra@users.noreply.github.com>
2024-06-12 10:53:33 -0300

    Ajusta o repositório de livro para funcionar de forma eficiente.

--- src/repositorio/EmprestimoRepositorio.java
+++ src/repositorio/EmprestimoRepositorio.java
@@ -2,24 +2,60 @@ package repositorio;
 
 import modelo.Emprestimo;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class EmprestimoRepositorio {
-    private final HashSet<Emprestimo> ER;
+  private final HashSet<Emprestimo> ER;
+  private final HashMap<String, ArrayList<Emprestimo>> matriculas_empretimos;
+  private final HashMap<String, ArrayList<Emprestimo>> isbns_empretimos;
 
-    public EmprestimoRepositorio() {
-        this.ER = new HashSet<Emprestimo>();
-    }
+  public EmprestimoRepositorio() {
+    this.ER = new HashSet<>();
+    this.matriculas_empretimos = new HashMap();
+    this.isbns_empretimos = new HashMap();
+  }
 
-    public boolean adicionaEmprestimo(Emprestimo emprestimo) {
-        return ER.add(emprestimo);
+  public boolean adicionaEmprestimo(Emprestimo emprestimo) {
+    if (!ER.add(emprestimo)) {
+      return false;
     }
+    if (matriculas_empretimos.containsKey(emprestimo.getMatricula())) {
+      matriculas_empretimos.get(emprestimo.getMatricula()).add(emprestimo);
+    } else {
+      ArrayList<Emprestimo> emprestimosMatricula = new ArrayList<>();
+      emprestimosMatricula.add(emprestimo);
+      matriculas_empretimos.put(emprestimo.getMatricula(), emprestimosMatricula);
+    }
+    if (isbns_empretimos.containsKey(emprestimo.getIsbn())) {
+      isbns_empretimos.get(emprestimo.getIsbn()).add(emprestimo);
+    } else {
+      ArrayList<Emprestimo> emprestimosIsbn = new ArrayList<>();
+      emprestimosIsbn.add(emprestimo);
+      isbns_empretimos.put(emprestimo.getIsbn(), emprestimosIsbn);
+    }
+    return true;
+  }
 
-    public boolean removeEmprestimo(Emprestimo emprestimo) {
-        return ER.remove(emprestimo);
+  public boolean removeEmprestimo(Emprestimo emprestimo) {
+    if (!ER.remove(emprestimo)) {
+      return false;
     }
+    matriculas_empretimos.remove(emprestimo.getMatricula());
+    isbns_empretimos.remove(emprestimo.getIsbn());
+    return true;
+  }
 
-    public HashSet<Emprestimo> getEmprestimos() {
-        return new HashSet<Emprestimo>(ER);
-    }
+  public HashSet<Emprestimo> getEmprestimos() {
+    return new HashSet<Emprestimo>(ER);
+  }
+
+  public ArrayList<Emprestimo> getEmprestimosPorMatricula(String matricula) {
+    return new ArrayList<>(matriculas_empretimos.get(matricula));
+  }
+
+  public ArrayList<Emprestimo> getEmprestimosIsbn(String isbn) {
+    return new ArrayList<>(isbns_empretimos.get(isbn));
+  }
 }
